5753d959345badf8705489162b26dbc024e9fd90
Igor Polshin <polshin.igor@sitronics-kt.ru>
2025-05-27 21:38:22 +0300

    Получение токена осуществляется через переменную окружения.

--- src/functions/atomic/stockrate_top_five.py
+++ src/functions/atomic/stockrate_top_five.py
@@ -1,6 +1,7 @@
 """Модуль с функцией для вывода курсов топ-5 ценных бумаг с использованием Finnhub API."""
 
 import json
+import os
 import requests
 from requests.exceptions import RequestException
 from telebot.types import Message
@@ -8,7 +9,7 @@ from bot_func_abc import AtomicBotFunctionABC
 
 class StockRateFunction(AtomicBotFunctionABC):
     commands = ["stockrate"]
-    authors = ["Igor"]
+    authors = ["Garik205"]
     about = "Вывод курсов топ-5 ценных бумаг"
     description = "Команда /stockrate показывает текущие курсы для топ-5 ценных бумаг."
     state = True
@@ -18,7 +19,10 @@ class StockRateFunction(AtomicBotFunctionABC):
         @bot.message_handler(commands=self.commands)
         def handle_stockrate(message: Message):
             try:
-                api_key = "d0picahr01qgccu9ahlgd0picahr01qgccu9ahm0"
+                api_key = os.getenv("FINNHUB_API_KEY")
+                if not api_key:
+                    bot.send_message(message.chat.id, "API ключ не найден. Пожалуйста, настройте переменную среды FINNHUB_API_KEY.")
+                    return
                 symbols = ['AAPL', 'GOOGL', 'AMZN', 'MSFT', 'TSLA']
                 stock_info = {
                     'AAPL': 'Apple Inc.',
