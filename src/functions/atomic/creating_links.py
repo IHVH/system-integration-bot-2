e64dbfd6cf097d8643b3f02e5a9ebe5c3e980d57
MrNightFox <210133000+MrNightFox@users.noreply.github.com>
2025-06-17 23:11:26 +0300

    fixed function creating_links

--- src/functions/atomic/creating_links.py
+++ src/functions/atomic/creating_links.py
@@ -1,7 +1,7 @@
 """Модуль создания коротких ссылок с возможностью собственного названия используя Free Url Shortener API"""
 
 from typing import List
-import time  #  Добавлено со связи возможным долгим ответом от сервера
+import time
 import telebot
 import requests
 from telebot import types
@@ -31,12 +31,13 @@ class AtomicExampleBotFunction(AtomicBotFunctionABC):
             except requests.exceptions.RequestException:
                 return None
 
-        @bot.message_handler(commands=["createlink"])
+        @bot.message_handler(commands=[self.commands[0]])
         def create_link(message: types.Message):
             '''Функция создания простой ссылки'''
             url = ' '.join(message.text.split()[1:])
             if not url:
-                bot.send_message(message.chat.id, 'Используйте: /createlink <URL>')
+                bot.send_message(message.chat.id,
+                f'Используйте: /{self.commands[0]} <URL>')
                 return
 
             api_url = f'https://ulvis.net/api.php?url={url}&private=1'
@@ -56,12 +57,13 @@ class AtomicExampleBotFunction(AtomicBotFunctionABC):
                     return
                 time.sleep(2)
 
-        @bot.message_handler(commands=["customlink"])
+        @bot.message_handler(commands=[self.commands[1]])
         def create_custom_link(message: types.Message):
             '''Функция создания простой ссылки со своим названием'''
             args = message.text.split()
             if len(args) < 3:
-                bot.send_message(message.chat.id, 'Используйте: /customlink <URL> <Кастомное имя>')
+                bot.send_message(message.chat.id,
+                f'Используйте: /{self.commands[1]} <URL> <Кастомное имя>')
                 return
 
             url = args[1]
@@ -83,33 +85,4 @@ class AtomicExampleBotFunction(AtomicBotFunctionABC):
                     bot.send_message(message.chat.id, 'Ошибка: превышено время ожидания.')
                     return
                 time.sleep(2)
-        '''
-        # Не реализовать, нет информации по примеру использования создания с паролем(ссылка создается без пароля).
-        @bot.message_handler(commands=["protectedlink"])
-        def create_protected_link(message: types.Message):
-            args = message.text.split()
-            if len(args) < 4:
-                bot.send_message(message.chat.id, 'Используйте: /protectedlink <URL> <Пароль> <Кастомное имя>')
-                return
 
-            url = args[1]
-            password = args[2]
-            custom_name = args[3]
-
-            api_url = f'https://ulvis.net/api.php?url={url}&custom={custom_name}&password={password}&private=1'
-            start_time = time.time()
-            timeout = 30  # Время ожидания в секундах
-
-            while True:
-                response = fetch_url(api_url)
-                if response and response.status_code == 200:
-                    bot.send_message(message.chat.id, f'Ответ от сервера: {response.text}')
-                    return
-                if response:
-                    bot.send_message(message.chat.id, f'Ошибка: {response.text}.')
-                    return
-                if time.time() - start_time > timeout:
-                    bot.send_message(message.chat.id, 'Ошибка: превышено время ожидания.')
-                    return
-                time.sleep(2)
-        '''
